968dcbfac173bdaff8a8b7fab6f24db163e45833
OmoruyiOhuoba <omoruyiohuoba@yahoo.com>
2020-09-07 01:13:11 +0100

    Updates

---
+++ precache-manifest.a8c3462573386bfad3735de617feca05.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba212637c0331a7bfca511d94758855",
+    "url": "/omoruyi-personal-website/index.html"
+  },
+  {
+    "revision": "b91883e860608a7799d7",
+    "url": "/omoruyi-personal-website/static/js/2.0b4521f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/omoruyi-personal-website/static/js/2.0b4521f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "916e74d1011c09c7e574",
+    "url": "/omoruyi-personal-website/static/js/main.f102e665.chunk.js"
+  },
+  {
+    "revision": "4d6534ba8e5816014357",
+    "url": "/omoruyi-personal-website/static/js/runtime-main.8c2c30c4.js"
+  },
+  {
+    "revision": "017e14f2682cd2ad9f38ea697e360234",
+    "url": "/omoruyi-personal-website/static/media/BloombankImg-1.017e14f2.png"
+  },
+  {
+    "revision": "72f6efe06c81da1c8090b6a49c993b80",
+    "url": "/omoruyi-personal-website/static/media/BloombankImg-2.72f6efe0.png"
+  },
+  {
+    "revision": "5e902dcfb98b9abeefad7457125c2aa5",
+    "url": "/omoruyi-personal-website/static/media/BloombankImg-4.5e902dcf.png"
+  },
+  {
+    "revision": "fa31dcac353bf972e463f1d08a2febba",
+    "url": "/omoruyi-personal-website/static/media/mountain.fa31dcac.png"
+  },
+  {
+    "revision": "ead775fae12cf7399a8de61359a31a3b",
+    "url": "/omoruyi-personal-website/static/media/myAvatar.ead775fa.png"
+  },
+  {
+    "revision": "41b07de0b1a64b35bb69264e405071b9",
+    "url": "/omoruyi-personal-website/static/media/pencil-ruler.41b07de0.png"
+  },
+  {
+    "revision": "837470625c8b70834437668988a7e856",
+    "url": "/omoruyi-personal-website/static/media/personal-1.83747062.png"
+  },
+  {
+    "revision": "2bbf5560f54b151a8e08574403e2f3ae",
+    "url": "/omoruyi-personal-website/static/media/personal-2.2bbf5560.png"
+  },
+  {
+    "revision": "a9e2af875741967fae3efe79b5187677",
+    "url": "/omoruyi-personal-website/static/media/personal-3.a9e2af87.png"
+  },
+  {
+    "revision": "b3206b45b8b8d2e074cc8da433ff7604",
+    "url": "/omoruyi-personal-website/static/media/straightTree.b3206b45.png"
+  },
+  {
+    "revision": "bc9b4bf9249b4be2aa9e9fd60ac296d8",
+    "url": "/omoruyi-personal-website/static/media/tatafoImg-1.bc9b4bf9.png"
+  },
+  {
+    "revision": "95add20f5b93b93ca49188869b4835e0",
+    "url": "/omoruyi-personal-website/static/media/tatafoImg-2.95add20f.png"
+  },
+  {
+    "revision": "f3a532a57e8c1df5ed25b1bff33f983d",
+    "url": "/omoruyi-personal-website/static/media/tatafoImg-3.f3a532a5.png"
+  },
+  {
+    "revision": "84cc4e99476b4e2fd6b54677b402e663",
+    "url": "/omoruyi-personal-website/static/media/uyi-face.84cc4e99.JPG"
+  }
+]);
